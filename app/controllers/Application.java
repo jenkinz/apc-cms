c8cd50c8359d6a775c4dd3a594bf43faa7d2eadb
Brian Jenkins <bj@jenkinz.com>
2012-05-29 21:28:22 -0700

    Added routes for testimonial CRUD

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,67 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
-  
+
   public static Result index() {
     return ok(index.render("Your new application is ready."));
   }
-  
+
+  /**
+   * Display all testimonials.
+   */
+  public static Result listTestimonials() {
+    return TODO;
+  }
+
+  /**
+   * Display the new testimonial form.
+   * 
+   * @return
+   */
+  public static Result createTestimonial() {
+    return TODO;
+  }
+
+  /**
+   * Handle the new testimonial form submission.
+   * 
+   * @return
+   */
+  public static Result saveTestimonial() {
+    return TODO;
+  }
+
+  /**
+   * Display the edit form of an existing testimonial.
+   * 
+   * @param id
+   * @return
+   */
+  public static Result editTestimonial(Long id) {
+    return TODO;
+  }
+
+  /**
+   * Handle the edit testimonial form submission.
+   * 
+   * @param id
+   * @return
+   */
+  public static Result updateTestimonial(Long id) {
+    return TODO;
+  }
+
+  /**
+   * Handle testimonial deletion.
+   * 
+   * @param id
+   * @return
+   */
+  public static Result deleteTestimonial(Long id) {
+    return TODO;
+  }
 }
