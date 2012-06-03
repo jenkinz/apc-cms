667e7a6197cb2a1fbefcad1cb4b2069f2d6c23bb
Brian Jenkins <bj@jenkinz.com>
2012-06-02 17:24:55 -0700

    Remove bootstrap dependency

--- project/Build.scala
+++ project/Build.scala
@@ -9,14 +9,12 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "com.github.twitter" % "bootstrap" % "2.0.3",
       "mysql" % "mysql-connector-java" % "5.1.18", // for DEV
       "postgresql" % "postgresql" % "8.4-702.jdbc4" // for PROD
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here
-      resolvers += "webjars" at "http://webjars.github.com/m2" // resolver for bootstrap
     )
 
 }
